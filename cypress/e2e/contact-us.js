fe8cd0f4a1552e4b6b44f1908cc56090f42f0a2a
Aveena Lingineni <praveenakunche@Aveenas-MacBook-Air.local>
2023-11-14 12:15:24 -0800

    created contact us

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,14 @@
+describe('Test Contact Us form via webdriverUnoi', () => {
+  it('Should be able to submit a suceesful submission via contact us form', () => {
+    cy.visit('/');
+    cy.contains('CONTACT US').invoke('removeAttr','target').click({force : true});
+    cy.url().should('include','contactus'); 
+    cy.get('[name="first_name"]').type('Praveena')
+    cy.get('[name="last_name"]').type('Kunche');
+    cy.get('[name="email"]').type('testmail@gmail.com');
+    cy.get('textarea.feedback-input').type('Hello world');
+    cy.get('[type="submit"]').click();
+    cy.contains('h1','Thank You for your Message!').should('be.visible') ;
+    cy.get('[id="fountainG"]').should('be.visible');
+  })
+})
